8c51a3d8fe874bf84accc9fb68ba7986f3060300
ranjithkumar1999 <46967082+ranjithkumar1999@users.noreply.github.com>
2019-01-24 06:33:15 -0800

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,9 +1,8 @@
-# your code goes here
 #largest of three number
 a,b,c=map(int,input().split())
 if a>b and a>c:
-	print("a")
+	print(a)
 elif b>c:
-	print("b")
+	print(b)
 else:
-	print("c")
+	print(c)
