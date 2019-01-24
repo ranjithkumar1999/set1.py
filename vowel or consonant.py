fb8fc41e9a28ded636e1143b1f16673fc519cd35
ranjithkumar1999 <46967082+ranjithkumar1999@users.noreply.github.com>
2019-01-24 06:10:48 -0800

    vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,9 @@
+# your code goes here
+#vowel or consonant_ranjith
+x=input()
+y=x.lower()
+if y=='a' or y=='e' or y=='i' or y=='o'or y=='u':
+	print("vowel")
+else:
+	print("consonant")
+# your code goes here
